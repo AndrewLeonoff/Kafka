c12a8640aab6662efab5a6ae87dbee648a77623c
Andrew Leonov <andrewleonov999@gmail.com>
2023-11-05 18:09:35 +0300

    Project completed

---
+++ producer.py
@@ -0,0 +1,19 @@
+from confluent_kafka import Producer
+
+def callback(err, event):
+    if err:
+        print(f'Produce to topic {event.topic()} failed for event: {event.key()}')
+    else:
+        val = event.value().decode('utf8')
+        print(f'{val} sent to partition {event.partition()}.')
+
+def say_hello(producer, key):
+    value = f'Hello {key}!'
+    producer.produce('hello_topic', value, key, on_delivery=callback)
+
+if __name__ == '__main__':
+    config = {'bootstrap.servers': 'localhost:9092'}
+    producer = Producer(config)
+    keys = ['Aleksey', 'Elena', 'Ivan', 'Anna', 'Pavel', 'Svetlana']
+    [say_hello(producer, key) for key in keys]
+    producer.flush()
